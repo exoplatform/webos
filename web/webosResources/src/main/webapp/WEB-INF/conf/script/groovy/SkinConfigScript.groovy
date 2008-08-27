a9caf218e210feda53fff0b12c4c6e808c3fb514
thuy_nguyen_ngoc <thuy_nguyen_ngoc@42d3c691-033b-0410-95b4-af486b8189e5>
2008-08-27 03:50:49 +0000

    webos-104: Splip Stylesheet from webosResource
    git-svn-id: http://svn.exoplatform.org/projects/webos/trunk@19108 42d3c691-033b-0410-95b4-af486b8189e5

--- web/webosResources/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
+++ web/webosResources/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -1,1 +1,3 @@
-SkinService.addPortalSkin("WebOSSkin", "Default", "/webosResources/skin/Stylesheet.css", ServletContext);
+SkinService.addPortalSkin("WebOSSkin", "Default", "/webosResources/skin/Defaultskin/Stylesheet.css", ServletContext);
+SkinService.addPortalSkin("WebOSSkin", "Mac", "/webosResources/skin/MacSkin/Stylesheet.css", ServletContext);
+SkinService.addPortalSkin("WebOSSkin", "Vista", "/webosResources/skin/VistaSkin/Stylesheet.css", ServletContext);
