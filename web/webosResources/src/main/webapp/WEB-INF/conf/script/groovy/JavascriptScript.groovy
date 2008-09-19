ff53e195d2812c886b2ee90d1a87a3a52e136e0e
tran <tran@42d3c691-033b-0410-95b4-af486b8189e5>
2008-09-19 10:27:36 +0000

    PORTAL-1935 separate webos from portal
    git-svn-id: http://svn.exoplatform.org/projects/webos/trunk@20160 42d3c691-033b-0410-95b4-af486b8189e5

--- web/webosResources/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
+++ web/webosResources/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
@@ -1,2 +1,5 @@
+/* Animation Javascripts */
+JavascriptService.addJavascript("eXo.animation.ImplodeExplode", "/javascript/eXo/animation/ImplodeExplode.js", ServletContext);
+
 JavascriptService.addJavascript("eXo.desktop.UIDesktop", "/javascript/eXo/desktop/UIDesktop.js", ServletContext);
 JavascriptService.addJavascript("eXo.desktop.UIDockbar", "/javascript/eXo/desktop/UIDockbar.js", ServletContext);
