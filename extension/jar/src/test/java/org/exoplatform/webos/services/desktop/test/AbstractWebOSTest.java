fa0132d360cf9ffc085fa8e1e84a481813f17370
hoang_to <hoang_to@42d3c691-033b-0410-95b4-af486b8189e5>
2010-12-16 09:22:57 +0000

    WEBOS-387: Create AbstractWebOSTest class
    git-svn-id: http://svn.exoplatform.org/projects/webos/trunk@64056 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ extension/jar/src/test/java/org/exoplatform/webos/services/desktop/test/AbstractWebOSTest.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2009 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.exoplatform.webos.services.desktop.test;
+
+import org.exoplatform.component.test.AbstractKernelTest;
+import org.exoplatform.component.test.ConfigurationUnit;
+import org.exoplatform.component.test.ConfiguredBy;
+import org.exoplatform.component.test.ContainerScope;
+
+/**
+ * 
+ *  This class provides default configuration of GateIn and WebOS backbone services. It is preferable 
+ * that JUnit tests of WebOS project extend <link>AbstractWebOSTest</link>  
+ * 
+ * @author Minh Hoang TO - hoang281283@gmail.com
+ *
+ * Dec 16, 2010
+ */
+@ConfiguredBy({
+	@ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/webos-desktop-service-configuration.xml"),
+	@ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/webos-jcr-configuration.xml"),
+	@ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/webos-portal-configuration.xml"),
+	@ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.test.jcr-configuration.xml"),
+  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.test.organization-configuration.xml")	
+})
+public abstract class AbstractWebOSTest extends AbstractKernelTest{
+
+	public AbstractWebOSTest() {
+		super();
+	}
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		
+		super.tearDown();
+	}
+}
